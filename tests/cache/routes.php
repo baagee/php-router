e5a66d867ac50350045d4505df641fb2fab73522
党留辉 <liuhuidang@sf-express.com>
2019-03-31 00:02:10 +0800

    增加缓存示例

---
+++ tests/cache/routes.php
@@ -0,0 +1,37 @@
+<?php
+// time:2019-03-30 16:01:08
+return array (
+  '/get/test1' => 
+  array (
+    'methods' => 
+    array (
+      0 => 'GET',
+    ),
+    'callback' => 'App@test1',
+    'other' => 
+    array (
+    ),
+  ),
+  '/get/test2' => 
+  array (
+    'methods' => 
+    array (
+      0 => 'GET',
+    ),
+    'callback' => 'App@test2',
+    'other' => 
+    array (
+    ),
+  ),
+  '/get/test3' => 
+  array (
+    'methods' => 
+    array (
+      0 => 'GET',
+    ),
+    'callback' => 'App@test2',
+    'other' => 
+    array (
+    ),
+  ),
+);
---
+++ tests/test4.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Desc:
+ * User: baagee
+ * Date: 2019/3/29
+ * Time: 下午5:24
+ */
+include_once __DIR__ . '/../vendor/autoload.php';
+
+class App
+{
+    public function test1($a)
+    {
+        echo __FUNCTION__;
+    }
+
+    public function test2($a)
+    {
+        echo __FUNCTION__;
+    }
+}
+
+// 添加路由的方法
+function addRouter()
+{
+    \BaAGee\Router\Router::get('/get/test1', 'App@test1');
+    \BaAGee\Router\Router::get('/get/test2', 'App@test2');
+    \BaAGee\Router\Router::get('/get/test3', 'App@test2');
+}
+
+// 是否开发模式
+$isDebug = false;
+// 如果不是开发模式(false)，设置一个路由缓存路径，
+//      如果缓存文件存在，会返回true，直接跳过，执行dispatch
+//      如果缓存文件不存在，会返回false,然后添加路由，最后执行dispatch，
+//      请求结束时将路由信息写入缓存文件，下次执行时文件存在，返回true，
+//      跳过添加路由，直接执行dispatch
+// 如果是开发模式(true)，每次都走添加路由的方法，然后执行dispatch
+if ($isDebug || \BaAGee\Router\Router::setCachePath(__DIR__ . '/cache') === false) {
+    echo '没有缓存';
+    addRouter();
+}
+
+\BaAGee\Router\Router::dispatch();
