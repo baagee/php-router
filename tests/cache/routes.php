43cdbb71a6e4dc2878fde6a1e607503095d995e0
党留辉 <liuhuidang@sf-express.com>
2019-04-08 16:33:37 +0800

    ..

--- tests/cache/routes.php
+++ tests/cache/routes.php
@@ -1,60 +1,56 @@
 <?php
 // time:2019-04-08 08:30:16
-return array (
-  'static' => 
-  array (
-    '/get/test1' => 
-    array (
-      'methods' => 
-      array (
-        0 => 'GET',
-      ),
-      'callback' => 'App@test1',
-      'other' => 
-      array (
-      ),
-    ),
-    '/get/test2' => 
-    array (
-      'methods' => 
-      array (
-        0 => 'GET',
-      ),
-      'callback' => 'App@test2',
-      'other' => 
-      array (
-      ),
-    ),
-  ),
-  'regexp' => 
-  array (
-    'g' => 
-    array (
-      '\\/get\\/(?<name>\\S+?)' => 
-      array (
-        'methods' => 
-        array (
-          0 => 'GET',
+return array(
+    'static' =>
+        array(
+            '/get/test1' =>
+                array(
+                    'methods'  =>
+                        array(
+                            0 => 'GET',
+                        ),
+                    'callback' => 'App@test1',
+                    'other'    =>
+                        array(),
+                ),
+            '/get/test2' =>
+                array(
+                    'methods'  =>
+                        array(
+                            0 => 'GET',
+                        ),
+                    'callback' => 'App@test2',
+                    'other'    =>
+                        array(),
+                ),
         ),
-        'callback' => 'App@test3',
-        'other' => 
-        array (
+    'regexp' =>
+        array(
+            'g' =>
+                array(
+                    '\\/get\\/(?<name>\\S+?)' =>
+                        array(
+                            'methods'  =>
+                                array(
+                                    0 => 'GET',
+                                ),
+                            'callback' => 'App@test3',
+                            'other'    =>
+                                array(),
+                        ),
+                ),
+            '/' =>
+                array(
+                    '\\/(?:(?<aaa>\\S+?))?' =>
+                        array(
+                            'methods'  =>
+                                array(
+                                    0 => 'GET',
+                                ),
+                            'callback' => 'App@test3',
+                            'other'    =>
+                                array(),
+                        ),
+                ),
         ),
-      ),
-    ),
-    '/' => 
-    array (
-      '\\/(?:(?<aaa>\\S+?))?' => 
-      array (
-        'methods' => 
-        array (
-          0 => 'GET',
-        ),
-        'callback' => 'App@test3',
-        'other' => 
-        array (
-        ),
-      ),
-    ),
-  ),
 );
